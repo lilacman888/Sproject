72047954ba858c3537ac71ee0998b97aba162592
lilac <lilacman888@gmail.com>
2020-07-16 17:37:36 +0900

    Sproject start

---
+++ src/main/java/com/sProject/board/boardDao.java
@@ -0,0 +1,19 @@
+package com.sProject.board;
+
+import java.sql.Date;
+
+public class boardDao {
+		private int num;
+		private String writer;
+		private String subject;
+		private String content;
+		private String email;
+		private int readcount;
+		private String passwd;
+		private int ref;
+		private int re_step;
+		private int re_level;
+		private String ip;
+		private Date reg_date;
+		private String del;
+}
